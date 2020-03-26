532381afa0fa89d1c269ecd47f33006a1941ef84
Tjalle <31376864+tjallo@users.noreply.github.com>
2020-03-26 15:53:45 +0100

    profit calculator finished

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
+#Import Dependencies
+import requests
+import json
+#Import Local Files
 from sources import secrets as s
+from resources import myBuys as mb
 
+baseUrl = "https://steamcommunity.com/market/priceoverview/?appid=730&currency=3&market_hash_name="
+results = []
+
+for i, item in enumerate(mb.items):
+    if item[0] == "":
+        url = baseUrl + item[1] + item[0]
+    else:
+        url = baseUrl + item[1] + "%20" + "(" + item[0] + ")"
+
+    url = url.replace(' ', '%20')    
+    
+    post = json.loads(requests.post(url).text)
+    	
+    results.append([post['lowest_price'][:-1].replace(',', '.'), item[1] + " " + item[0], item[2]])
+
+totalProfit = 0
+
+for item in results:
+    profit = float(item[0]) - float(item[2])
+    print(f"You have made €{profit} on {item[1]}")
+    totalProfit += profit
+
+print(f"Your total profit is {totalProfit}")
---
+++ resources/myBuys.py
@@ -0,0 +1,8 @@
+items = [
+    ["", "Sticker | chrisJ | Berlin 2019", 0.07],
+    ["Battle-Scarred", "StatTrak™ M4A1-S | Flashback", 2.59],
+    ["Minimal Wear", "StatTrak™ AK-47 | Uncharted", 3.74],
+    ["Minimal Wear", "StatTrak™ UMP-45 | Arctic Wolf", 2.30],
+    ["Well-Worn", "StatTrak™ USP-S | Cortex", 10.42],
+    ["Field-Tested", "StatTrak™ AWP | Atheris", 14.35],
+]
